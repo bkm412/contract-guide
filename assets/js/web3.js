96ee9866dd755c3fd55feecf513e1fdb1958162f
BangKyungmin <km.bang@linkercoin.com>
2019-03-19 18:10:33 +0900

    테스트용 function 모두 infura provider로 변경

--- assets/js/web3.js
+++ assets/js/web3.js
@@ -1,51 +1,46 @@
 import Web3 from 'web3';
 import abi from '../json/abi';
 
-let web3Object, Contract;
-if(typeof window !== 'undefined'){
+let web3Object, Contract, globalWeb3Object, globalContract;
+if(typeof window !== 'undefined' && window.web3){
     web3Object = new Web3(window.web3.currentProvider);
     Contract = new web3Object.eth.Contract(abi , "0x1d406FFCA0d61df0e659887B2F501ead6Bb96426" );
 }
 
+const provider = new Web3.providers.HttpProvider(
+    "https://kovan.infura.io/v3/f00d7a0545114321b4b6b8ea54f4feb0"
+);
+
+globalWeb3Object = new Web3(provider);
+globalContract = new globalWeb3Object.eth.Contract(abi , "0x1d406FFCA0d61df0e659887B2F501ead6Bb96426" );
+const testAddress = "0x1283798F83dd68d23C91344b1677315608e6864B"
 
 export const getFactoryID = async() => {
-    const account = await web3Object.eth.getAccounts();
-    return await Contract.methods.GetFactoryId().call({
-        from : account[0]
+    return await globalContract.methods.GetFactoryId().call({
+        from : testAddress
     });
 };
 
 export const getFactoryInfo = async() => {
-    const account = await web3Object.eth.getAccounts();
-    return await Contract.methods.GetFactoryInfo(1).call({
-        from : account[0]
+    return await globalContract.methods.GetFactoryInfo(1).call({
+        from : testAddress
     })
 };
 
-export const addFactory = async(address, name, location, detail) => {
-    const account = await web3Object.eth.getAccounts();
-    return await Contract.methods.AddFactory(address, name, location, detail).send({
-        from : account[0]
-    })
-}
-
 export const editFactoryInfo = async() => {
-    const account = await web3Object.eth.getAccounts();
-    return await Contract.methods.EditFactoryInfo().call({
-        from : account[0]
+    return await globalContract.methods.EditFactoryInfo().call({
+        from : testAddress
     })
 }
 
 export const getChimneyInfo = async() => {
-    const account = await web3Object.eth.getAccounts();
-    return await Contract.methods.GetChimneyInfo(1).call({
-        from : account[0]
+    return await globalContract.methods.GetChimneyInfo(1).call({
+        from : testAddress
     })
 }
 
 export const getRecordData = async() => {
-    const account = await web3Object.eth.getAccounts();
-    return await Contract.methods.GetRecordData(10,1,1).call({
-        from : account[0]
+    return await globalContract.methods.GetRecordData(10,1,1).call({
+        from : testAddress
     })
 }
